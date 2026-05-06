d2ba6ded105d70827709234f4feaa52072ceddf0
stbsoftware <stbsoftware2k@gmail.com>
2026-05-06 03:37:08 -0500

    Revert "Commit 2026-05-02 14:39"
    This reverts commit 6b15bebdcaa488f18ab4206083132ac61bfe4a5e.

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -2,4 +2,4 @@ export const PHONE_E164 = "+573001659072";
 export const PHONE_DISPLAY = "(+57) 300 165 9072";
 /** Abre WhatsApp Web o la app para chatear con este número. */
 export const WHATSAPP_URL = "https://wa.me/573001659072";
-export const EMAIL = "info@stbsoftware.com";
+export const EMAIL = "info@stbsoftware.co";
